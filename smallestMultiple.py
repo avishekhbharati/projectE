6269c360729c528d7abd880539128b39e348c1d1
avishekh <avishekh.bharati@gmail.com>
2016-08-05 08:12:06 +0545

    smallest multiple solved . but yet to optimize

--- smallestMultiple.py
+++ smallestMultiple.py
@@ -1,41 +1,18 @@
+#takes a long time. Need to optimize
+import datetime
+
 def samallestMultiple():
     numTest = 11
     while True:
-        for i in range(2, 11):
-            if numTest%i != 0:                
+        for i in range(2, 21):
+            if numTest%i != 0 :                
                 break
-            elif i == 10:
-                print(numTest)       
+            elif i == 20:
+                return numTest
                 
-        numTest = numTest + 1        
-
-samallestMultiple()
-
-'''
-for each number starting from 2521
-    check if number is divisible by all of the numbers from 1 to 20
-
-def divisibleByAll(x):
-    flag = False
-    for i in range(1,20):
-        if x%i != 0 :
-            return False
-        else :
-            
+        numTest = numTest + 1
         
+print(datetime.datetime.now())
+print(samallestMultiple())
+print(datetime.datetime.now())
 
-def smallestMultiple():    
-    numToTest = 2520
-    while True:
-        for i in range(1, 20):
-            if numToTest % i != 0:
-                 break;
-            if i == 20 and numToTest % i == 0:
-                return numToTest
-                break
-            
-                
-        numToTest += 2
-
-print('smallest : '+ str(smallestMultiple()))
-'''
